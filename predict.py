d2fdb1ac4b54ce9224963e9005a8c3489de2a4b4
panhaiqi <978005581@qq.com>
2018-01-20 16:36:03 +0800

    忘了归一化

--- predict.py
+++ predict.py
@@ -5,11 +5,13 @@ from keras.applications.mobilenet import relu6,DepthwiseConv2D
 from skimage import img_as_ubyte,transform
 import numpy as np
 from sys import argv
+import numpy as np
 
-def predict(argv):
+def predict(argv[0]):
     data = []
     data.append(img_as_ubyte(transform.resize(argv, (224,224,3))))
-
+    data = data.astype('float32')
+    data /= 255.
 
     model = load_model('mobilenet_add.hdf5',custom_objects={
                   'relu6': relu6,
