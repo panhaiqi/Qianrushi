7c973ebdd2666e1011626e7c6021414260ce816c
panhaiqi <978005581@qq.com>
2018-01-20 21:22:09 +0800

    pygame播语音合成的片段声音会发生很奇怪的变化。。。暂时骚操作一下吧

--- load_music.py
+++ load_music.py
@@ -1,6 +1,14 @@
 import pygame
-
-def load_music(music):
+import os
+def load_music(music,synthesis):
     pygame.mixer.init()
-    track = pygame.mixer.music.load(music)
+    pygame.mixer.music.load(music)
     pygame.mixer.music.play()
+    # pygame.mixer.music.load(synthesis)
+    # pygame.mixer.music.play()
+    os.system(synthesis)
+
+
+
+# def load_music(music,synthesis):
+#     os.system(music)
