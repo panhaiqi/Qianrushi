8efc049f43d8294dd538836d917737e8736c6610
panhaiqi <978005581@qq.com>
2018-01-20 17:34:38 +0800

    大概是考试考傻了，出了一堆非常智障的bug

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from load_poem import load_poem
 from load_music import load_music
 from send_gcode import send_gcode
 from sys import argv
+from skimage import io
 base_path = r'..\\Qianrushi\\'
-
-predict_result = predict(argv)
-poem_num,add_path = load_poem(predict_result)
+img = io.imread(argv[1])
+predict_result = predict(img)
+poem_num = load_poem(predict_result)
 music_path = base_path + 'music' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.mp3'
-gcode_path = base_path + 'gcode' + '\\'+ str(picture_class) +'\\'+ str(random_num) + '.txt'
+gcode_path = base_path + 'gcode' + '\\'+ str(predict_result) +'\\'+ str(poem_num) + '.txt'
 load_music(music_path)
 send_gcode(gcode_path)
--- predict.py
+++ predict.py
@@ -1,16 +1,23 @@
 #-*-coding:utf-8 * -
 from keras.models import Model, load_model
-from LoadData import load_data
+#from LoadData import load_data
 from keras.applications.mobilenet import relu6,DepthwiseConv2D
 from skimage import img_as_ubyte,transform
 import numpy as np
-from sys import argv
+#from sys import argv
 import numpy as np
 from skimage import color
-def predict(argv[0]):
-    img = color.gray2rgb(argv[0])
+
+def predict(img):
     data = []
-    data.append(img_as_ubyte(transform.resize(img, (224,224,3))))
+    #img = color.gray2rgb(argv)
+    #data = []
+    #data.append(img_as_ubyte(transform.resize(argv, (224,224,3))))
+    #img = io.imread(argv[1])
+    img = color.gray2rgb(img)
+    img = img_as_ubyte(transform.resize(img, (224,224,3)))
+    data.append(img)
+    data = np.array(data)
     data = data.astype('float32')
     data /= 255.
 
