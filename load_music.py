2d1fcd99f6219dce9bb0343a1a1e42d7bf30114f
panhaiqi <978005581@qq.com>
2018-01-22 02:04:19 +0800

    珍爱生命远离MP3

--- load_music.py
+++ load_music.py
@@ -1,12 +1,20 @@
 import pygame
+
 import os
 def load_music(music,synthesis):
+    # song = pygame.mixer.Sound(music)
+    #
+    # while True:
+    #     song.play()
     pygame.mixer.init()
+    #channel1 = pygame.mixer.find_channel()
     pygame.mixer.music.load(music)
     pygame.mixer.music.play()
     # pygame.mixer.music.load(synthesis)
     # pygame.mixer.music.play()
-    os.system(synthesis)
+    song = pygame.mixer.Sound(synthesis)
+    song.play()
+    #os.system(synthesis)
 
 
 
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from load_music import load_music
 from send_gcode import send_gcode
 from sys import argv
 from skimage import io
+import threading
 base_path = r'..\\Qianrushi\\'
 img = io.imread(argv[1])
 predict_result = predict(img)
 poem_num = load_poem(predict_result)
-music_path = base_path + 'music' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.mp3'
-synthesis_path = base_path + 'synthesis' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.mp3'
+music_path = base_path + 'music' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.ogg'
+synthesis_path = base_path + 'synthesis' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.wav'
 gcode_path = base_path + 'gcode' + '\\'+ str(predict_result) +'\\'+ str(poem_num) + '.txt'
 load_music(music_path,synthesis_path)
 #load_music(synthesis_path)
