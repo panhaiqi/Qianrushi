3cd3e7e38af90ebc61f8e8e9e11093b6496ac0b9
Pan Haiqi <panhaiqi15@gmail.com>
2018-03-14 21:42:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from load_music import load_music
 from send_gcode import send_gcode
 from sys import argv
 from skimage import io
-import threading
+from load import load
+from image_process import image_process
+import numpy as np
+# import datetime
 base_path = r'..\\Qianrushi\\'
-img = io.imread(argv[1])
-predict_result = predict(img)
-poem_num = load_poem(predict_result)
-music_path = base_path + 'music' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.ogg'
-synthesis_path = base_path + 'synthesis' + '\\'+ str(predict_result)+'\\'+str(poem_num)+'.wav'
-gcode_path = base_path + 'gcode' + '\\'+ str(predict_result) +'\\'+ str(poem_num) + '.txt'
-load_music(music_path,synthesis_path)
-#load_music(synthesis_path)
-send_gcode(gcode_path)
+model = load()
+while (1):
+    img = input()
+
+    data = image_process(img)
+
+    predict_result = np.argmax(model.predict(data))
+
+    poem_num = load_poem(predict_result)
+    music_path = base_path + 'music' + '\\' + str(predict_result) + '\\' + str(poem_num) + '.ogg'
+    synthesis_path = base_path + 'synthesis' + '\\' + str(predict_result) + '\\' + str(poem_num) + '.wav'
+    gcode_path = base_path + 'gcode' + '\\' + str(predict_result) + '\\' + str(poem_num) + '.txt'
+    load_music(music_path, synthesis_path)
+    send_gcode(gcode_path)
